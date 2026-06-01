59eb700cc9003bcb58015c6e2523ab4e905105f2
anann5 <anann5@hotmail.com>
2026-06-01 03:22:25 +0300

    Refactor server.js to include fs module
    fix syntax error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 5850;
