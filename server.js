b797d483ff0b0b804e7e22e0d3d40bfda44ce9c6
anan <anann5@hotmail.com>
2026-06-04 13:03:19 +0300

    Fix categories images and layout of back to main button

--- server.js
+++ server.js
@@ -17,13 +17,20 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             defaultSrc: ["'self'"],
-            styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
-            fontSrc: ["'self'", "https://fonts.gstatic.com"],
+            styleSrc: ["'self'", "'unsafe-inline'", 
+                "https://fonts.googleapis.com",
+                "https://cdnjs.cloudflare.com"],
+            fontSrc: ["'self'", 
+                "https://fonts.gstatic.com",
+                "https://cdnjs.cloudflare.com"],
             imgSrc: ["'self'", "data:", "https:"],
-            scriptSrc: ["'self'", "'unsafe-inline'"], // عدّله لاحقاً إذا أضفت CDN
+            scriptSrc: ["'self'", "'unsafe-inline'",
+                "https://www.paypal.com",
+                "https://cdnjs.cloudflare.com"],
+            frameSrc: ["https://www.paypal.com"],
         },
     },
-    crossOriginEmbedderPolicy: false, // مهم لعدم كسر الصور
+    crossOriginEmbedderPolicy: false,
 }));
 
 // ====================================================
